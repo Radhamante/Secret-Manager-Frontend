94bdbcde0f8250b9d1814805d9424aa9a16132b4
Radhamante <raphael.delassus@proton.me>
2025-03-06 17:34:23 +0100

    fix : environement use .fr

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiURL:
-    (window as any)['env']['API_URL'] || 'https://api.secret.radhamante.xyz',
+    (window as any)['env']['API_URL'] || 'https://api.secret.radhamante.fr',
 };
